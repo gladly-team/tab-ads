c721e44af26e30568a74ef719743793c0f80f504
Kevin Jennison <kevin.jennison1@gmail.com>
2020-02-03 11:53:01 -0800

    Add Babel and a build script

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+export const fetchAds = config => {
+  console.log('Called `fetchAds` with config:', config)
+}
+
+export const displayAd = adId => {
+  console.log('Called `displayAd` with adId:', adId)
+}
