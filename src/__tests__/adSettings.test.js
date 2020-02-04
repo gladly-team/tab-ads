1a5e141e7d6ba7c4f12af6d8e5a958442e01575a
Kevin Jennison <kevin.jennison1@gmail.com>
2020-02-03 17:24:18 -0800

    Update some tests

--- src/__tests__/adSettings.test.js
+++ src/__tests__/adSettings.test.js
@@ -2,13 +2,8 @@
 
 import moment from 'moment'
 import MockDate from 'mockdate'
-import {
-  getBrowserExtensionInstallTime,
-  hasUserDismissedAdExplanation,
-} from 'src/utils/local-user-data-mgr'
 
 jest.mock('js/utils/experiments')
-jest.mock('js/utils/local-user-data-mgr')
 
 const mockNow = '2017-05-19T13:59:58.000Z'
 
@@ -36,66 +31,9 @@ describe('ad settings', () => {
     expect(HORIZONTAL_AD_SLOT_DOM_ID).toBe('div-gpt-ad-1464385677836-0')
   })
 
-  test('[recently-installed] shows 1 ad', () => {
-    getBrowserExtensionInstallTime.mockReturnValue(
-      moment(mockNow).subtract(23, 'hours')
-    )
-    const { getNumberOfAdsToShow } = require('src/adSettings')
-    expect(getNumberOfAdsToShow()).toEqual(1)
-  })
-
-  test('[unknown-recently-installed] shows 3 ads when the install time is missing', () => {
-    getBrowserExtensionInstallTime.mockReturnValue(null)
+  test('shows 3 ads', () => {
     const { getNumberOfAdsToShow } = require('src/adSettings')
     expect(getNumberOfAdsToShow()).toEqual(3)
-  })
-
-  test('[no-recently-installed] shouldShowOneAd returns false', () => {
-    getBrowserExtensionInstallTime.mockReturnValue(
-      moment(mockNow).subtract(10, 'days')
-    )
-    const { shouldShowAdExplanation } = require('src/adSettings')
-    expect(shouldShowAdExplanation()).toEqual(false)
-  })
-
-  test('[no-recently-installed] shouldShowAdExplanation returns false', () => {
-    getBrowserExtensionInstallTime.mockReturnValue(
-      moment(mockNow).subtract(10, 'days')
-    )
-    const { shouldShowAdExplanation } = require('src/adSettings')
-    expect(shouldShowAdExplanation()).toEqual(false)
-  })
-
-  test('[unknown-recently-installed] shouldShowAdExplanation returns false', () => {
-    getBrowserExtensionInstallTime.mockReturnValue(null)
-    const { shouldShowAdExplanation } = require('src/adSettings')
-    expect(shouldShowAdExplanation()).toEqual(false)
-  })
-
-  test('[recently-installed] shouldShowAdExplanation returns true', () => {
-    getBrowserExtensionInstallTime.mockReturnValue(
-      moment(mockNow).subtract(12, 'seconds')
-    )
-    const { shouldShowAdExplanation } = require('src/adSettings')
-    expect(shouldShowAdExplanation()).toEqual(true)
-  })
-
-  test('[recently-installed] [no-dismissed] shouldShowAdExplanation returns true', () => {
-    getBrowserExtensionInstallTime.mockReturnValue(
-      moment(mockNow).subtract(12, 'seconds')
-    )
-    hasUserDismissedAdExplanation.mockReturnValue(false)
-    const { shouldShowAdExplanation } = require('src/adSettings')
-    expect(shouldShowAdExplanation()).toEqual(true)
-  })
-
-  test('[recently-installed] [dismissed] shouldShowAdExplanation returns false', () => {
-    getBrowserExtensionInstallTime.mockReturnValue(
-      moment(mockNow).subtract(12, 'seconds')
-    )
-    hasUserDismissedAdExplanation.mockReturnValue(true)
-    const { shouldShowAdExplanation } = require('src/adSettings')
-    expect(shouldShowAdExplanation()).toEqual(false)
   })
 
   test('getVerticalAdSizes returns the expected ad sizes', () => {
