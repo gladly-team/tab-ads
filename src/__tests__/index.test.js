8df92dddd67102559e1b4cc8de40f8c5f3b015d4
Kevin Jennison <kevin.jennison1@gmail.com>
2020-02-05 14:01:51 -0800

    Add tests for index.js

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,30 @@
+/* eslint-env jest */
+
+import getAds from 'src/ads'
+
+jest.mock('src/ads')
+jest.mock('src/utils/logger')
+
+describe('index.js', () => {
+  it('contains the expected exports', () => {
+    const index = require('src/index')
+    expect(index.fetchAds).toBeDefined()
+    expect(index.fetchAds).toEqual(expect.any(Function))
+  })
+
+  it('fetches ads with no config', async () => {
+    expect.assertions(2)
+    const { fetchAds } = require('src/index')
+    await fetchAds()
+    expect(getAds).toHaveBeenCalledTimes(1)
+    expect(getAds).toHaveBeenCalledWith(undefined)
+  })
+
+  it('fetches ads with the config, when provided', async () => {
+    expect.assertions(1)
+    const { fetchAds } = require('src/index')
+    const myConfig = { foo: 'bar' }
+    await fetchAds(myConfig)
+    expect(getAds).toHaveBeenCalledWith(myConfig)
+  })
+})
