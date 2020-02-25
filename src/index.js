09c59d644464885fddeaafcaaf6b219718d731e1
Kevin Jennison <kevin.jennison1@gmail.com>
2020-02-24 16:08:03 -0800

    Remove console log

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@
 import getAds from 'src/fetchAds'
 
 export const fetchAds = async config => {
-  // eslint-disable-next-line no-console
-  console.log(`Called "fetchAds" with config: ${JSON.stringify(config)}`)
   await getAds(config)
 }
 
