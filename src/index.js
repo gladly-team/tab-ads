a484e2f480c381e6d6e1bf8d379de7413d74dfaf
Kevin Jennison <kevin.jennison1@gmail.com>
2020-02-24 16:54:15 -0800

    Export getAllWinningBids

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 /* eslint-disable import/prefer-default-export */
 import getAds from 'src/fetchAds'
+import { getAllWinningBids as getAllWinningBidsForAds } from 'src/utils/getWinningBids'
 
 export const fetchAds = async config => {
   await getAds(config)
 }
 
-export const getAllWinningBids = () => {
-  // eslint-disable-next-line no-console
-  console.log('TODO')
-}
+export const getAllWinningBids = getAllWinningBidsForAds
 
 // Expose functions to the global variable that are helpful
 // for debugging in devtools.
