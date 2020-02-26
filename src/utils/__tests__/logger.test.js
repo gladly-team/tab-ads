85c1fa2e863cbaa1c402408bea26a3ae0d801746
Kevin Jennison <kevin.jennison1@gmail.com>
2020-02-26 14:15:44 -0800

    Remove commented-out code

--- src/utils/__tests__/logger.test.js
+++ src/utils/__tests__/logger.test.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-console */
 /* eslint-env jest */
 
-// import { setConfig } from 'src/config'
 import { getMockTabAdsUserConfig } from 'src/utils/test-utils'
 
 afterEach(() => {
