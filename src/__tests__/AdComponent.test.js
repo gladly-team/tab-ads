59cb04c8ffed7f51496accc29e052bc438d7dab9
Kevin Jennison <kevin.jennison1@gmail.com>
2020-02-25 16:27:03 -0800

    Add test

--- src/__tests__/AdComponent.test.js
+++ src/__tests__/AdComponent.test.js
@@ -1,4 +1,5 @@
 /* eslint-env jest */
+/* eslint-disable react/jsx-props-no-spreading */
 
 import React from 'react'
 import { render } from '@testing-library/react'
@@ -6,12 +7,31 @@ import { render } from '@testing-library/react'
 // https://github.com/testing-library/jest-dom#table-of-contents
 import '@testing-library/jest-dom/extend-expect'
 
+const getMockProps = () => ({
+  adId: 'my-wonderful-ad-id',
+  style: undefined,
+})
+
 describe('Ad component', () => {
   it('renders a child with a div ID equal to the adId prop', () => {
     expect.assertions(1)
     const AdComponent = require('src/AdComponent').default
-    const adId = 'my-ad-id'
-    const { getByTestId } = render(<AdComponent adId={adId} />)
-    expect(getByTestId('ad-container')).toHaveAttribute('id', adId)
+    const mockProps = getMockProps()
+    const { getByTestId } = render(<AdComponent {...mockProps} />)
+    expect(getByTestId('ad-container')).toHaveAttribute('id', mockProps.adId)
+  })
+
+  it('sets the style on the root element', () => {
+    expect.assertions(1)
+    const AdComponent = require('src/AdComponent').default
+    const mockProps = {
+      ...getMockProps(),
+      style: {
+        display: 'inline',
+        backgroundColor: 'green',
+      },
+    }
+    const { container } = render(<AdComponent {...mockProps} />)
+    expect(container.firstChild).toHaveStyle(mockProps.style)
   })
 })
