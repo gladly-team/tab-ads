cd38553e8cb8e36d5184efc19e9911260a02f507
Kevin Jennison <kevin.jennison1@gmail.com>
2020-02-26 09:54:17 -0800

    Export AdComponent

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 /* eslint-disable import/prefer-default-export */
 import getAds from 'src/fetchAds'
 import { getAllWinningBids as getAllWinningBidsForAds } from 'src/utils/getWinningBids'
+import ReactAdComponent from 'src/AdComponent'
 
 export const fetchAds = async config => {
   await getAds(config)
 }
 
 export const getAllWinningBids = getAllWinningBidsForAds
+
+export const AdComponent = ReactAdComponent
 
 // Expose functions to the global variable that are helpful
 // for debugging in devtools.
