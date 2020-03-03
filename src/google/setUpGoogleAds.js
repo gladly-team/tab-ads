3f5ec185ca3e24eb0ce2b96f3ae81fb3b350f99f
Kevin Jennison <kevin.jennison1@gmail.com>
2020-03-02 16:21:36 -0800

    Make Google ad setup rely on provided ad units

--- src/google/setUpGoogleAds.js
+++ src/google/setUpGoogleAds.js
@@ -1,35 +1,22 @@
 import getGoogleTag from 'src/google/getGoogleTag'
 
-// @feature/configurable-ad-count
-// TODO: assumes we are showing all 3 ads. Make that configurable.
 export default config => {
-  const {
-    leaderboard,
-    rectangleAdPrimary,
-    rectangleAdSecondary,
-  } = config.newTabAds
+  const { adUnits } = config
+  if (!adUnits.length) {
+    return
+  }
   const googletag = getGoogleTag()
+
+  // Set up each ad unit.
+  adUnits.forEach(adUnit => {
+    googletag.cmd.push(() => {
+      googletag
+        .defineSlot(adUnit.adUnitId, adUnit.sizes, adUnit.adId)
+        .addService(googletag.pubads())
+    })
+  })
+
   googletag.cmd.push(() => {
-    // Leaderboard
-    googletag
-      .defineSlot(leaderboard.adUnitId, leaderboard.sizes, leaderboard.adId)
-      .addService(googletag.pubads())
-    // Rectangle #1
-    googletag
-      .defineSlot(
-        rectangleAdPrimary.adUnitId,
-        rectangleAdPrimary.sizes,
-        rectangleAdPrimary.adId
-      )
-      .addService(googletag.pubads())
-    // Rectangle #2
-    googletag
-      .defineSlot(
-        rectangleAdSecondary.adUnitId,
-        rectangleAdSecondary.sizes,
-        rectangleAdSecondary.adId
-      )
-      .addService(googletag.pubads())
     googletag.pubads().enableSingleRequest()
     googletag.enableServices()
   })
