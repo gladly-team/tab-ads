515c39b230b09735765de2b9658d1d68774f6ee8
Kevin Jennison <kevin.jennison1@gmail.com>
2020-03-25 15:25:02 -0700

    Load bidders earlier

--- src/__tests__/getBidders.test.js
+++ src/__tests__/getBidders.test.js
@@ -1,22 +1,29 @@
 /* eslint-env jest */
 
-import prebidBidder from 'src/providers/prebid/prebidBidder'
-import amazonBidder from 'src/providers/amazon/amazonBidder'
-import indexExchangeBidder from 'src/providers/indexExchange/indexExchangeBidder'
-import getBidders from 'src/getBidders'
-import { isClientSide } from 'src/utils/ssr'
-
 jest.mock('src/providers/amazon/getAmazonTag')
 jest.mock('src/providers/prebid/prebidBidder')
 jest.mock('src/providers/amazon/amazonBidder')
 jest.mock('src/utils/ssr')
 
 beforeEach(() => {
+  // Default to rendering on the client side.
+  const { isClientSide } = require('src/utils/ssr')
   isClientSide.mockReturnValue(true)
+})
+
+afterEach(() => {
+  jest.resetModules()
 })
 
 describe('bidders', () => {
   it('returns the expected enabled bidders on the client side', () => {
+    const { isClientSide } = require('src/utils/ssr')
+    isClientSide.mockReturnValue(true)
+    const prebidBidder = require('src/providers/prebid/prebidBidder').default
+    const amazonBidder = require('src/providers/amazon/amazonBidder').default
+    const indexExchangeBidder = require('src/providers/indexExchange/indexExchangeBidder')
+      .default
+    const getBidders = require('src/getBidders').default
     expect(getBidders()).toEqual([
       prebidBidder,
       amazonBidder,
@@ -25,7 +32,9 @@ describe('bidders', () => {
   })
 
   it('returns an empty array when not on the client side', () => {
+    const { isClientSide } = require('src/utils/ssr')
     isClientSide.mockReturnValue(false)
+    const getBidders = require('src/getBidders').default
     expect(getBidders()).toEqual([])
   })
 })
--- src/getBidders.js
+++ src/getBidders.js
@@ -1,21 +1,24 @@
 import { isClientSide } from 'src/utils/ssr'
 
+let bidders = []
+
+// Bidder code (like Prebid) can rely on the window variable,
+// so we can't include when rendering server-side.
+if (isClientSide()) {
+  const prebidBidder = require('src/providers/prebid/prebidBidder').default
+  const amazonBidder = require('src/providers/amazon/amazonBidder').default
+  const indexExchangeBidder = require('src/providers/indexExchange/indexExchangeBidder')
+    .default
+  bidders = [prebidBidder, amazonBidder, indexExchangeBidder]
+}
+
 /**
  * On the client side, return an array of active bidders.
  * In a server environment, return an empty array.
  * @return {Bidder[]}
  */
 const getBidders = () => {
-  // Bidder cdoe (like Prebid) can rely on the window variable,
-  // so we can't include when rendering server-side.
-  if (isClientSide()) {
-    const prebidBidder = require('src/providers/prebid/prebidBidder').default
-    const amazonBidder = require('src/providers/amazon/amazonBidder').default
-    const indexExchangeBidder = require('src/providers/indexExchange/indexExchangeBidder')
-      .default
-    return [prebidBidder, amazonBidder, indexExchangeBidder]
-  }
-  return []
+  return bidders
 }
 
 export default getBidders
