9a4a827c9376224fb80b46c9de2a6a70c866dbe4
Kevin Jennison <kevin.jennison1@gmail.com>
2021-07-15 16:23:37 -0400

    Revert "One approach"
    This reverts commit da72540bdffa258785f194b8a1bf175ed4085416.

--- src/__tests__/getAvailableAdUnits.test.js
+++ src/__tests__/getAvailableAdUnits.test.js
@@ -1,40 +1,6 @@
 /* eslint-env jest */
 
-import getAvailableAdUnits, { getAdUnitConfig } from 'src/getAvailableAdUnits'
-
-describe('getAdUnitConfig', () => {
-  it('exports the expected ad units config', () => {
-    const expectedVal = {
-      leaderboard: {
-        // The long leaderboard ad.
-        adId: 'div-gpt-ad-1464385677836-0',
-        adUnitId: '/43865596/HBTL',
-        // Sizes sent to bidders.
-        sizes: [[728, 90]],
-        // Sizes allowed to be served from the ad server. This
-        // cab include more options so that, e.g., custom in-house
-        // ad campaigns can have unique sizes but programmatic
-        // bidders do not.
-        allowableAdSlotSizes: [[728, 90]],
-      },
-      rectangleAdPrimary: {
-        // The primary rectangle ad (bottom-right).
-        adId: 'div-gpt-ad-1464385742501-0',
-        adUnitId: '/43865596/HBTR',
-        sizes: [[300, 250]],
-        allowableAdSlotSizes: [[300, 250]],
-      },
-      rectangleAdSecondary: {
-        // The second rectangle ad (right side, above the first).
-        adId: 'div-gpt-ad-1539903223131-0',
-        adUnitId: '/43865596/HBTR2',
-        sizes: [[300, 250]],
-        allowableAdSlotSizes: [[300, 250]],
-      },
-    }
-    expect(getAdUnitConfig()).toEqual(expectedVal)
-  })
-})
+import getAvailableAdUnits from 'src/getAvailableAdUnits'
 
 describe('getAvailableAdUnits', () => {
   it('exports the expected structure of ad units', () => {
--- src/getAvailableAdUnits.js
+++ src/getAvailableAdUnits.js
@@ -1,53 +1,20 @@
-const adUnitConfig = {
+export default () => ({
   leaderboard: {
     // The long leaderboard ad.
     adId: 'div-gpt-ad-1464385677836-0',
     adUnitId: '/43865596/HBTL',
-    // Sizes sent to bidders.
     sizes: [[728, 90]],
-    // Sizes allowed to be served from the ad server. This
-    // cab include more options so that, e.g., custom in-house
-    // ad campaigns can have unique sizes but programmatic
-    // bidders do not.
-    allowableAdSlotSizes: [[728, 90]],
   },
   rectangleAdPrimary: {
     // The primary rectangle ad (bottom-right).
     adId: 'div-gpt-ad-1464385742501-0',
     adUnitId: '/43865596/HBTR',
     sizes: [[300, 250]],
-    allowableAdSlotSizes: [[300, 250]],
   },
   rectangleAdSecondary: {
     // The second rectangle ad (right side, above the first).
     adId: 'div-gpt-ad-1539903223131-0',
     adUnitId: '/43865596/HBTR2',
     sizes: [[300, 250]],
-    allowableAdSlotSizes: [[300, 250]],
   },
-}
-
-export const getAdUnitConfig = () => adUnitConfig
-
-// Format the ad unit config into the expected shape used
-// by the API method `getAvailableAdUnits`.
-// Includes properties: adId, adUnitId, sizes
-const availableAdUnits = Object.keys(adUnitConfig).reduce(
-  (newConfig, currentKey) => {
-    const itemConfig = adUnitConfig[currentKey]
-    const { adId, adUnitId, sizes } = itemConfig
-
-    // eslint-disable-next-line no-param-reassign
-    newConfig[currentKey] = {
-      adId,
-      adUnitId,
-      sizes,
-    }
-    return newConfig
-  },
-  {}
-)
-
-const getAvailableAdUnits = () => availableAdUnits
-
-export default getAvailableAdUnits
+})
